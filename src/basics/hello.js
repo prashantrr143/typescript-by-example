a25223b2b05235a88f728f163336f5232e1d5628
Prashant Kumar Singh <prashant.siet153@gmail.com>
2019-04-08 17:11:32 +0530

    Delete hello.js

--- src/basics/hello.js
+++
@@ -1,7 +0,0 @@
-var message = 'Hello World Message from Typescript';
-function helloWorld(message) {
-    console.log(" Typescript Returns ==> " + message);
-}
-helloWorld("Learning");
-helloWorld(message);
-//# sourceMappingURL=hello.js.map
