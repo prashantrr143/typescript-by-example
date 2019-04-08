1110fd4a2188fd3268f1a0e7a026ffe75d480dbd
Prashant Kumar Singh <prashant.sirt153@gmail.com>
2019-04-08 17:08:37 +0530

    Hello program added with tsconfig

---
+++ src/basics/hello.js
@@ -0,0 +1,7 @@
+var message = 'Hello World Message from Typescript';
+function helloWorld(message) {
+    console.log(" Typescript Returns ==> " + message);
+}
+helloWorld("Learning");
+helloWorld(message);
+//# sourceMappingURL=hello.js.map
---
+++ src/basics/hello.ts
@@ -0,0 +1,13 @@
+
+let message : string = 'Hello World Message from Typescript';
+
+function helloWorld(message: string ){
+    console.log(` Typescript Returns ==> ${message}` );
+}
+
+helloWorld(`Learning`);
+helloWorld(message);
+
+
+
+
